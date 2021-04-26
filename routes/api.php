f2d67d963bd195b4ffa10aa88c3a3f28158b33db
Jose Maria Farfan <magoalakai@gmail.com>
2021-04-26 18:57:14 +0200

    Breeze y JWT instalados, y Api con routes de auth creada

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +23,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Routes de JWT integradas en Routes de BREEZE para autenticación
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+
+Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+            ->name('password.email');
+
+Route::post('/reset-password', [NewPasswordController::class, 'store'])
+            ->name('password.update');
+
+Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+            ->middleware(['auth', 'throttle:6,1'])
+            ->name('verification.send');
+
+Route::post('/confirm-password', [ConfirmablePasswordController::class, 'store'])
+            ->middleware('auth');
+
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+            ->middleware('auth')
+            ->name('logout');
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::post('user',[UserController::class, 'getAuthenticatedUser']);
 });
